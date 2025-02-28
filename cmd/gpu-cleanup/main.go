32756fa7f31611eebb8cefa5b40db65aeaa90cdf
Arjun <arjun.gadiyar@gmail.com>
2025-02-28 00:02:05 +0000

    make image 3

--- cmd/gpu-cleanup/main.go
+++ cmd/gpu-cleanup/main.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
@@ -13,12 +29,12 @@ import (
 
 func main() {
 	// Set up the Kubernetes client
-	config, err := clientcmd.BuildConfigFromFlags("", "") // connects to the kubeconfig file which is used by kubectl and other clients like client-go to interact with the Kubernetes API server
+	config, err := clientcmd.BuildConfigFromFlags("", "")
 	if err != nil {
 		log.Fatalf("Failed to build config: %v", err)
 	}
 	// Create the clientset
-	clientset, err := kubernetes.NewForConfig(config) // a clientset is a client for the Kubernetes API server and is used to interact with the API server
+	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		log.Fatalf("Failed to create clientset: %v", err)
 	}
